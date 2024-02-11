530f364f20fcff51e151925b3d89c82f0f77b9e2
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-02-11 19:27:04 +0100

    sprite looks in walking direction

--- src/player.rs
+++ src/player.rs
@@ -5,13 +5,21 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, player_setup);
-        app.add_systems(Update, player_movement);
+        app.add_systems(Update, (player_movement, player_animation_handler));
     }
 }
 
 #[derive(Component)]
-struct Player;
+struct Player {
+    direction: Vec3,
+    player_animation: PlayerAnimation,
+}
 
+enum PlayerAnimation {
+    Walking
+}
+
+// Basic Starting Player setup 
 fn player_setup(
     mut commands: Commands,
     window_query: Query<&Window>,
@@ -31,35 +39,63 @@ fn player_setup(
             transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
             ..default()
         },
-        Player {},
+        Player {
+            direction: Vec3::ZERO,
+            player_animation: PlayerAnimation::Walking,
+        },
     ));
 }
 
+fn player_animation_handler(
+    mut player_query: Query<(
+        &mut Player,
+        &mut TextureAtlasSprite
+    )>
+) {
+    for (mut player, mut sprite) in &mut player_query {
+        match player.player_animation {
+            PlayerAnimation::Walking => {
+                match player.direction {
+                    _ => {},
+                }
+            },
+        }
+    }
+}
+
+// Handeling Player Movement Up Down Left Right
 fn player_movement(
+    mut animation_query: Query<&mut TextureAtlasSprite>,
     mut query: Query<&mut Transform, With<Player>>,
     time: Res<Time>,
     key_input: Res<Input<KeyCode>>,
 ) {
-    if let Ok(mut transform) = query.get_single_mut() {
-        let mut direction = Vec3::ZERO;
+    if let Ok(mut sprite) = animation_query.get_single_mut() {
+        if let Ok(mut transform) = query.get_single_mut() {
+            let mut direction = Vec3::ZERO;
 
-        if key_input.pressed(KeyCode::Left) || key_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-1., 0., 0.);
-        } 
-        if key_input.pressed(KeyCode::Right) || key_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1., 0., 0.);
-        } 
-        if key_input.pressed(KeyCode::Up) || key_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0., 1., 0.);
-        } 
-        if key_input.pressed(KeyCode::Down) || key_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0., -1., 0.);
+            if key_input.pressed(KeyCode::Left) || key_input.pressed(KeyCode::A) {
+                direction += Vec3::new(-1., 0., 0.);
+                sprite.index = 3;
+            } 
+            if key_input.pressed(KeyCode::Right) || key_input.pressed(KeyCode::D) {
+                direction += Vec3::new(1., 0., 0.);
+                sprite.index = 1;
+            } 
+            if key_input.pressed(KeyCode::Up) || key_input.pressed(KeyCode::W) {
+                direction += Vec3::new(0., 1., 0.);
+                sprite.index = 7;
+            } 
+            if key_input.pressed(KeyCode::Down) || key_input.pressed(KeyCode::S) {
+                direction += Vec3::new(0., -1., 0.);
+                sprite.index = 5;
+            }
+
+            if direction.length() > 0. {
+                direction = direction.normalize();
+            }
+
+            transform.translation += direction * 100. * time.delta_seconds();
         }
-
-        if direction.length() > 0. {
-            direction = direction.normalize();
-        }
-
-        transform.translation += direction * 100. * time.delta_seconds();
     }
 }
