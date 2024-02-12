da867f0ddb26a58626cc2bfcd6c1179fdd3f67da
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-02-12 17:00:22 +0100

    start dungeon

---
+++ src/dungeon_generator.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+
+pub struct DungeonGeneratorPlugin;
+
+impl Plugin for DungeonGeneratorPlugin {
+    fn build(&self, app: &mut App) {
+
+    }
+}
+
+// TODO: Implement that god damm dungeon after some need playtesting
